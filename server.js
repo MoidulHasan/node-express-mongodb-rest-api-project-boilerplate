cfc867800b53afcaa427d89f4a72fcac1011d58b
Antukhan <antu.khan.988@gmail.com>
2023-05-16 01:26:34 +0600

    feat: mongoose configuration added

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
+Promise = require('bluebird');
 const app = require('./src/app.js')
 const { port, env } = require('./src/config/vars.js');
 const logger = require('./src/config/logger.js');
+const mongoose = require('./src/config/mongoose.js');
 
 
+// open mongoose connection
+mongoose.connect();
 
 
 app.listen(port, () => {
     logger.info(`Server is running on http://localhost:${port}`)
 })
+
+
+module.exports = app;
